c6bb183911c59308747addba7dd4798b872b307a
actionchain <actionchain@chainguard.dev>
2026-06-16 19:11:08 +0000

    Update alexwilson/enable-github-automerge-action@3.0.0

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import * as github from "@actions/github";
+import { setFailed, getInput } from "@actions/core";
+import {
+  EnableGithubAutomergeAction,
+  Options,
+} from "./enable-github-automerge-action";
+
+export async function run() {
+  try {
+    const { context } = github;
+    const options: Options = Object.create(null);
+
+    const token = getInput("github-token", { required: true });
+    const client = github.getOctokit(token);
+
+    const preferredMergeMethod = getInput("merge-method", { required: false });
+    if (preferredMergeMethod) {
+      options.preferredMergeMethod = preferredMergeMethod;
+    }
+
+    const automergeAction = new EnableGithubAutomergeAction(
+      client,
+      context,
+      options,
+    );
+    await automergeAction.run();
+  } catch (error) {
+    let message = error instanceof Error ? error.message : String(error);
+    setFailed(message);
+  }
+}
+
+run();
